7e7109230a0554cbba5da40bad411c71b912cd35
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-30 22:54:37 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import HeaderComponent from './components/header/HeaderComponent.vue';
+
+export default {
+  name:"MyApp",
+  data(){
+    return {
+
+    }
+  },
+  components:{
+    HeaderComponent,
+  }
+}
+</script>
+
+<template>
+  <header>
+    <!-- header component -->
+    <HeaderComponent />
+  </header>
+
+  <main>
+    <!-- main component -->
+      <!-- grid component -->
+        <!-- card component -->
+  </main>
+
+  <footer>
+    <!-- footer component -->
+  </footer>
+</template>
+
+<style lang="scss">
+@use "./assets/style/main.scss";
+
+
+</style>
---
+++ src/components/header/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name:"Header",
+    data(){
+
+    },
+    methods:{
+
+    },
+    components:{
+
+    }
+}
+</script>
+
+<template>
+<div>
+
+</div>
+</template>
+
+<style lang="scss" scoped>
+@import "../../assets/style/partials/variables";
+
+div{
+    background-color: $primary-color;
+    width: 100vw;
+    height: 100px;
+}
+</style>
